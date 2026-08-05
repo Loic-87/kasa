840068a2928dff92d7532d00e9f77530dd16f607
Loic-87 <loic.lf87@gmail.com>
2026-08-05 03:07:09 +0200

    Ajout du composant Header avec logo et styles SCSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
+import logo from '../../assets/LOGO.svg'
+import './Header.scss'
 
 function Header() {
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <img src={logo} alt="Kasa" className="header-logo" />
+      <nav className="header-nav">
         <Link to="/">Accueil</Link>
         <Link to="/about">À propos</Link>
       </nav>
