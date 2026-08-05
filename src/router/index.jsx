989e3d66a7e230be0fd2a234e87313c473193eca
Loic-87 <loic.lf87@gmail.com>
2026-08-05 02:14:13 +0200

    Ajout du composant Layout et intégration dans le Router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,16 +3,19 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import Logement from '../pages/Logement'
 import Error from '../pages/Error'
+import Layout from '../components/Layout'
 
 function Router() {
   return (
   <BrowserRouter>
+  <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/logement/:id" element={<Logement />} />
         <Route path="*" element={<Error />} />
       </Routes>
+  </Layout>
   </BrowserRouter>
 )
 }
