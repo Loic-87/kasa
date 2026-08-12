70a1d148d3fa0a7a000b3d2eeab16e288bfabaf2
Loic-87 <loic.lf87@gmail.com>
2026-08-12 16:55:26 +0200

    Page 404 pixel-perfect (404, message, lien, espaces mobile-first)

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
+import './Error.scss'
+
 function Error() {
   return (
-    <main>
-      <h1>404 - Page non trouvée</h1>
+    <main className="error">
+      <h1 className="error-title">404</h1>
+      <p className="error-message">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error-link">
+        Retourner sur la page d'accueil
+      </Link>
     </main>
   )
 }
